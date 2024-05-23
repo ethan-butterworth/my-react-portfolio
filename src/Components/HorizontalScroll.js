251381f08269e004670d639376dc4966b98571dd
EthanButterworth <136074583+EthanButterworth@users.noreply.github.com>
2024-05-23 15:15:05 +0100

    e

--- src/Components/HorizontalScroll.js
+++ src/Components/HorizontalScroll.js
@@ -58,4 +58,16 @@ const scroll = (direction) => {
   });
 }
 
+function transformScroll(event) {
+  if (!event.deltaY) {
+    return;
+  }
+
+  event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
+  event.preventDefault();
+}
+
+var element = document.scrollingElement || document.documentElement;
+element.addEventListener('wheel', transformScroll);
+
 export default HorizontalScroll;
