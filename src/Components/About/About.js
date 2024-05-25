334385b878cc7a25fb9b87eb591d681af636aa3a
EthanButterworth <136074583+EthanButterworth@users.noreply.github.com>
2024-05-25 13:56:32 +0100

    e

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import './About.css'
+import mypic from '../../Assets/My-Pic.jpg'
 
 const About = () => {
   return (
     <div>
         <section className="section">
-          <h2>Professional Summary:</h2>
-          <p>With over five years in IT Support, I excel at diagnosing and resolving tech issues, ensuring smooth operations, and providing excellent customer service. I'm adept at managing installations and quickly learning new technologies to enhance user satisfaction.</p>
-          <h2>Career Goals:</h2>
-          <p>I'm now looking to move into a front-end developer role, using my problem-solving skills, technical know-how, and passion for building and improving web solutions.</p>
-          <h2>Hobbies and Interests:</h2>
+        <img src={mypic} alt='A Picture of Ethan.' />
+          <h2>I'm a:</h2>
+          <ul>
+            <li>Support Engineer at Zuto.</li>
+            <li>Self taught Web Developer.</li>
+            <li>Passionate designer and creator.</li>
+          </ul>
+          <h2>My Journey:</h2>
           <p>My tech fascination first began with the Halo series on Xbox, which led me to build my own PC. These days, I'm passionate about Warhammer, enjoying the assembly, painting, and strategy of the hobby.</p>
+          <p>With over five years in IT Support, I excel at diagnosing and resolving tech issues, ensuring smooth operations, and providing excellent customer service. I'm adept at managing installations and quickly learning new technologies to enhance user satisfaction.</p>
+          <p>I'm now looking to move into a front-end developer role, using my problem-solving skills, technical know-how, and passion for building and improving web solutions.</p>
+          
         </section>
     </div>
   )
--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import './Landing.css';
-import mypic from '../../Assets/My-Pic.jpg'
+import landing from '../../Assets/Landing.jpg'
 
 const Landing = () => {
   return (
     <div>
-        <section className="section">
+        <section className="section" style={{ backgroundImage: `url(${landing})` }}>
           <p>Nice to meet you, I'm...</p>
           <h1>Ethan Butterworth</h1>
           <p>...an aspiring Web Developer.</p>
-          <img src={mypic} alt='A Picture of Ethan.' />
-          <h2>I'm a:</h2>
-          <ul>
-            <li>Support Engineer at Zuto.</li>
-            <li>Self taught Web Developer.</li>
-            <li>Passionate designer and creator.</li>
-          </ul>
         </section>
       </div>
   );
