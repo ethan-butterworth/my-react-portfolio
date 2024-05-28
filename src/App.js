028aaba9ceed19b0806bc7f77b2ea3c437789c9c
EthanButterworth <136074583+EthanButterworth@users.noreply.github.com>
2024-05-28 17:46:21 +0100

    e

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Landing from './Components/Landing/Landing.js';
+import Landing from './Components/Landing/Landing.js'
 import Nav from './Components/Navbar/Nav.js'
 import About from './Components/About/About.js'
-import Journey from './Components/Journey/Journey.js';
+import Journey from './Components/Journey/Journey.js'
 import Projects from './Components/Projects/Projects.js'
 import Links from './Components/Links/Links.js'
 import Footer from './Components/Footer/Footer.js'
+import Quote from './Components/Quote/Quote.js'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Landing />
       <Nav />
       <About />
+      <Quote />
       <Journey />
       <Projects />
       <Links />
---
+++ src/Components/Quote/Quote.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Quote.css'
+
+const Quote = () => {
+  return (
+    <div>
+        <section className='section quote'>
+            <div className='container'>
+                <h2>"While it is always best to believe in oneself, a little help from others can be a great blessing."</h2>
+                <p> — Uncle Iroh</p>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Quote
